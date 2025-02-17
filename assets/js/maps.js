30592ca693f073a791dd63ef2327866683a2224e
JonOwenWilliams <jonowenwilliams2468@gmail.com>
2025-02-17 21:23:48 +0000

    added google maps with a location to main page

---
+++ assets/js/maps.js
@@ -0,0 +1,12 @@
+function initMap() {
+    var location = { lat: 52.923165, lng: -1.476831 };
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        title: "Code Cuisine Location"
+    });
+}
