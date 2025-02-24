d87e80d8e0b2f6158e7cde1a64c20ea51bb4ef3f
JonOwenWilliams <jonowenwilliams2468@gmail.com>
2025-02-24 17:41:49 +0000

    added more styles to the booking page

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///booking.db'
 @app.route("/")
 def index():
     return render_template("index.html")
