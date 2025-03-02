ec5880bf3c2ea1e27a84b8ec223868be32e72e9d
JonOwenWilliams <jonowenwilliams2468@gmail.com>
2025-03-02 14:35:28 +0000

    added the login feature to the login page and asoon as the login is complete it retched booking data and displays them on the table on the next page

--- static/js/admin.js
+++ static/js/admin.js
@@ -9,3 +9,29 @@ function showAdminLogin() {
     document.getElementById("admin-login-section").style.display = "block";
     document.getElementById("admin-dashboard").style.display = "none";
 }
+
+//admin login
+function loginAdmin() {
+    let username = document.getElementById("admin-username").value;
+    let password = document.getElementById("admin-password").value;
+
+    fetch('/admin_login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            document.getElementById("admin-login-section").style.display = "none";
+            document.getElementById("admin-dashboard").style.display = "block";
+            fetchAdminBookings();
+        } else {
+            alert("Invalid login credentials");
+        }
+    })
+    .catch(error => {
+        console.error("Admin Login Error:", error);
+    })
+
+}
