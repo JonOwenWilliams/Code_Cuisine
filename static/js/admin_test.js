d3e76b4e7e867195550b49fc441e9d949d50cb7e
JonOwenWilliams <jonowenwilliams2468@gmail.com>
2025-03-04 23:19:15 +0000

    added a test to check if when admin selects a new date the table refreshes with new data

--- static/js/admin_test.js
+++ static/js/admin_test.js
@@ -1,3 +1,4 @@
+// Test incorrect admin loggins
 function testAdminLogin() {
     console.log("Testing admin login...");
 
@@ -18,6 +19,40 @@ function testAdminLogin() {
     })
     .catch(error => console.error("Error testing admin login", error));
 }
+// test for fetching admin bookings
+function testFetchAdminBookings() {
+    console.log("Test Fetch Admin Bookings...");
+
+    fetch('/api/bookings')
+    .then(response => response.json())
+    .then(bookings => {
+        if (!Array.isArray(bookings)) {
+            console.log("Fail: API did not return an array");
+        } else if (bookings.length === 0){
+            console.log("Pass: No bookings exist");
+        } else {
+            console.log(`Pass: ${bookings.length} bookings retrieved`);
+        }
+    })
+    .catch(error => console.error("Error testing FetchAdminBookings", error));
+}
+// Testing admin date selector
+function testAdminDateSelector() {
+    console.log("Testing admin date selector...");
+
+    let testDate = "2025-03-10";
+    document.getElementById("admin-booking-date").value = testDate;
+    fetchAdminBookings();
+
+    setTimeout(() => {
+        let tableRows = document.querySelectorAll("#admin-booking-list tr");
+        if (tableRows.length > 0) {
+            console.log("Pass: date selection updates bookings");
+        } else {
+            console.log("Fail: No bookings loaded after date selection")
+        }
+    }, 2000);
+}
 
 // function to run all tests
 
@@ -25,7 +60,10 @@ function runAllTests () {
 
     testAdminLogin();
 
-    setTimeout(() => { }, 2000)
+    setTimeout(() => {testFetchAdminBookings();}, 2000)
+
+    setTimeout(() => {testAdminDateSelector();}, 4000)
+
 }
 
 document.addEventListener("DOMContentLoaded", runAllTests);
